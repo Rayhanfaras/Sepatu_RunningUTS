c51603cb84c641cc5a4b264744c1d7b6decb612d
Rayhan Faras <ryhanfrasrmdhani@gmail.com>
2026-05-05 21:45:28 +0700

    Menambahkan url

--- appsmarketplace/lib/core/constants/api_constants.dart
+++ appsmarketplace/lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'http://10.110.115.90:8081/v1';
+  static const String baseUrl = 'http://192.168.43.90:8081/v1';
 
   // Auth endpoints
   static const String verifyToken = '/auth/verify-token';
